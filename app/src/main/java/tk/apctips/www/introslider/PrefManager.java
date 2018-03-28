358d138e0170f183d2982f7d58f7aca7eb927c00
Ankit Kumar <developerankitkumar@users.noreply.github.com>
2018-03-28 15:02:54 +0530

    Initial commit

---
+++ app/src/main/java/tk/apctips/www/introslider/PrefManager.java
@@ -0,0 +1,37 @@
+package tk.apctips.www.introslider;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by ankit on 27-03-2018.
+ */
+
+public class PrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "Intro Slider";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+}
